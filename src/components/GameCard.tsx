d290728c456db80ccb24cfac4e521f5a24c0e002
Ian Lustre <ianlustre16@gmail.com>
2026-06-15 17:37:37 +0800

    created game cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Games } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Games;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={12} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
