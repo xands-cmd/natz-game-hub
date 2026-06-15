362251336504e3895615d21f40ff2795bab18641
Ian Lustre <ianlustre16@gmail.com>
2026-06-15 14:49:06 +0800

    added switch to mode

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,13 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
