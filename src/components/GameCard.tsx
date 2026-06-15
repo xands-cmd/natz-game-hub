b1abffa540e2109fb89cdab64673040aa7cf10bf
Ian Lustre <ianlustre16@gmail.com>
2026-06-15 19:51:58 +0800

    display genre icons too

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Games } from "../hooks/useGames";
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Games;
@@ -11,18 +12,23 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image
+          src={getCroppedImageUrl(game.background_image)}
+          alt={game.name}
+        />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
